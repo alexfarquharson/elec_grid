cd8fe393014e594aa30e824b7f110e3802f7f9de
Alexander Farquharson <farquharsonar@gmail.com>
2025-09-18 14:55:42 +0100

    feature/org

---
+++ notebooks/todo.md
@@ -0,0 +1,5 @@
+1. Built weather prediction model on madrid data
+2. Change to use live met office solar data
+3. Change to live api vibes
+4. Change to predict solar generation at location x
+5. 
---
+++ src/first_model_build/main.py
@@ -0,0 +1,13 @@
+from xgb_train import main as xgb_main
+from sarima_train import main as sarima_main
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def get_outputs():
+    df_pred_xgb = xgb_main()
+    df_pred_sarima = sarima_main()
+
+    df_preds = pd.DataFrame(zip(df_pred_sarima['actuals'], df_pred_sarima['predictions'],df_pred_xgb['temp_pred'].reset_index(drop=True)),columns = ['actuals','sarima','xgb'])
+    df_preds = df_preds.iloc[-25:]
+    plt.plot(df_preds,label = ['actual','sarima','xgb'])
+    plt.legend()
