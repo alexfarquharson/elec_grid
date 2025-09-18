4c746d00baab9446b322016547335f2ed6c73ac8
Alexander Farquharson <farquharsonar@gmail.com>
2025-09-18 17:23:12 +0100

    feature.org: doronell data

---
+++ scripts/data_helpers/metoffice_api_helpers.py
@@ -0,0 +1,33 @@
+import sys
+import requests
+
+def met_office_forecast_api(baseUrl, timesteps, requestHeaders, latitude, longitude, excludeMetadata, includeLocation):
+    
+    url = baseUrl + timesteps 
+    
+    headers = {'accept': "application/json"}
+    headers.update(requestHeaders)
+    params = {
+        'excludeParameterMetadata' : excludeMetadata,
+        'includeLocationName' : includeLocation,
+        'latitude' : latitude,
+        'longitude' : longitude
+        }
+
+    success = False
+    retries = 5
+
+    while not success and retries >0:
+        try:
+            req = requests.get(url, headers=headers, params=params)
+            success = True
+        except Exception as e:
+            retries -= 1
+            time.sleep(10)
+            if retries == 0:
+                sys.exit()
+
+    req.encoding = 'utf-8'
+
+    print(req.text)
+    return req 
